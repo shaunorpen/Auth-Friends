a95344aff0a752c0614250388aafa82ccf6c51b3
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2019-10-15 13:51:10 +0100

    Friend list working

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, Route } from 'react-router-dom';
 
-function NavBar() {
+function NavBar(props) {
+  const isLoggedIn = localStorage.getItem('token');
+
+  const handleLogout = e => {
+    localStorage.clear();
+    props.history.push('/');
+  }
+
   return (
     <ul>
-      <li><Link to='/'>
-        Login
+      <li><Link to='/' onClick={handleLogout} >
+        {isLoggedIn ? 'Logout' : 'Login'}
       </Link></li>
       <li><Link to='/friends'>
         Friend List
@@ -15,7 +22,7 @@ function NavBar() {
   )
 }
 
-function Login() {
+function Login(props) {
   const initialUserDetails = { username: '', password: '', };
   const [userDetails, setUserDetails] = useState(initialUserDetails);
   
@@ -31,6 +38,7 @@ function Login() {
     axios.post('http://localhost:5000/api/login', userDetails)
       .then(res => {
         localStorage.setItem('token', res.data.payload);
+        props.history.push('/friends');
       })
       .catch(err => {
         alert(err.response.data.error);
@@ -49,21 +57,41 @@ function Login() {
   );
 }
 
+function axiosWithAuth() {
+  const token = localStorage.getItem('token');
+  const instance = axios.create({
+    headers: {
+      Authorization: token,
+    },
+  });
+  return instance;
+}
+
 function Friends() {
+  const [friendList, setFriendList] = useState([]);
+  useEffect(() => {
+    axiosWithAuth().get('http://localhost:5000/api/friends')
+    .then(res => {
+      setFriendList(res.data);
+    })
+    .catch(err => {
+      alert(err.response.data.error);
+    });
+  }, []);
   return (
     <div>
       <h2>Friend List</h2>
       <ul>
-        <Friend />
+        {friendList.map(friend => <Friend friend={friend} />)}
       </ul>
     </div>
   );
 }
 
-function Friend() {
+function Friend({ friend: { name, age, email } }) {
   return (
     <li>
-      Friend
+      {`${name} is ${age} years old and contactable at ${email}.`}
     </li>
   );
 }
