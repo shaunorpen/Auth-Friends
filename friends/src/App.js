0c3677a7ebc988f83e9b38f7a4f9bfe2d3d086fc
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2019-10-15 12:51:48 +0100

    Add app scaffold

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <ul>
+      <li><Link to='/'>
+        Login
+      </Link></li>
+      <li><Link to='/friends'>
+        Friends List
+      </Link></li>
+    </ul>
+  )
+}
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <label htmlFor='username'>Username: </label>
+      <input name='username' />
+      <label htmlFor='password'>Password: </label>
+      <input name='password' />
+      <button>Submit</button> 
+    </div>
+  );
+}
+
+function Friends() {
+  return (
+    <div>
+      <h2>Friend List</h2>
+      <Friend />
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div>
+      Friend
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <NavBar />
+      <Login />
+      <Friends />
     </div>
   );
 }
