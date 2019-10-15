78569538abeb66eefaa3dbcbba4d77be494b495a
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2019-10-15 13:22:41 +0100

    Get token, save to local storage

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link, Route } from 'react-router-dom';
 
 function NavBar() {
@@ -15,14 +16,35 @@ function NavBar() {
 }
 
 function Login() {
+  const initialUserDetails = { username: '', password: '', };
+  const [userDetails, setUserDetails] = useState(initialUserDetails);
+  
+  const handleChange = e => {
+    setUserDetails({
+      ...userDetails,
+      [e.target.name]: e.target.value,
+    });
+  } 
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', userDetails)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+      })
+      .catch(err => {
+        alert(err.response.data.error);
+      });
+  }
+
   return (
     <div>
       <h2>Login</h2>
       <label htmlFor='username'>Username: </label>
-      <input name='username' />
+      <input name='username' type='text' value={userDetails.username} onChange={handleChange} />
       <label htmlFor='password'>Password: </label>
-      <input name='password' />
-      <button>Submit</button> 
+      <input name='password' type='password' value={userDetails.password} onChange={handleChange} />
+      <button onClick={handleSubmit} >Submit</button> 
     </div>
   );
 }
